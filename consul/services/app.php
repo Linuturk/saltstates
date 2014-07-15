08a6118c987178a0bd1fa1680706a91827eb90e1
Justin Phelps <linuturk@onitato.com>
2014-07-15 13:56:12 -0500

    small update to the app

--- consul/services/app.php
+++ consul/services/app.php
@@ -4,7 +4,7 @@ echo "<h1>app: ".$_SERVER['SERVER_NAME']."</h1>";
 
 $ips = exec('dig @127.0.0.1 -p 8600 mysql.service.iad.consul +short');
 
-echo "<h1>db:</h1>";
+echo "<h1>db:$ips</h1>";
 foreach ($ips as $ip) {
   echo "<h2>$ip</h2>";
 }
