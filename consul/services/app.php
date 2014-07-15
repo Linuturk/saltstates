0437bf1d7ef3ef3e7ad7b8b4a6cf66660f1a487c
Justin Phelps <linuturk@onitato.com>
2014-07-15 14:54:26 -0500

    adding in a hostname

--- consul/services/app.php
+++ consul/services/app.php
@@ -2,8 +2,10 @@
 
 echo "<h1>app: ".$_SERVER['SERVER_NAME']."</h1>";
 
-$ips = exec('dig @127.0.0.1 -p 8600 mysql.service.iad.consul +short');
+$hostname = exec('hostname');
+echo "<h2>$hostname</h2>";
 
+$ips = exec('dig @127.0.0.1 -p 8600 mysql.service.iad.consul +short');
 echo "<h1>db: $ips</h1>";
 
 ?>
